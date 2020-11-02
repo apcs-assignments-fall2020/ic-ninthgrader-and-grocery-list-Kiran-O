56598ba65042e54c14a5a1c0e583fd01298ee6ee
Kiran-O <22kokelly@athenian.org>
2020-11-02 19:54:03 +0000

    Passed Ninth Grader Tests

--- src/main/java/NinthGrader.java
+++ src/main/java/NinthGrader.java
@@ -1,3 +1,17 @@
-public class NinthGrader {
+public class NinthGrader extends Student{
+    public NinthGrader(String a, String b){
+        super(a,b,2024);
+    }
+    public NinthGrader(){
+        super("Brad","King",2024);
+    }
+    public void doSomething(){
+        System.out.println(this.getFirstName() + "I am studying");
+    }
+    public String toString(){
+        return (this.getFirstName() + this.getLastName() + "," + 2024 + "(Ninth Grade)");
+    }
+
+
     
 }
